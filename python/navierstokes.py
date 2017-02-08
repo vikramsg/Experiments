393c41e1bde8768b2a424ed1be537b2372005b66
VSingh <vikramsingh001@gmail.com>
2017-02-08 19:54:22 +0200

    ENH: Completed function for getting manufactured solution for 2D NS

--- python/navierstokes.py
+++ python/navierstokes.py
@@ -12,6 +12,112 @@ def get_divergence(u, x, r):
 
     return temp
 
+
+
+def get_cns_2d(): 
+    '''
+    Create a manufactured solution for compressbile NS
+    '''
+    x = smp.DeferredVector('x')
+    t = smp.symbols('t')
+
+    dimension = 2
+
+    v     = (1, -0.5         )#Velocity
+    p     = 1                 #Pressure
+    gamma = 1.4               
+    mu    = 0.100             #Dynamic viscosity  
+    Pr    = 0.72              #Prandtl number
+    R     = 287               #Gas constant
+    Cv    = R/(gamma - 1)     #Gas constant
+
+    rho = 1 + 0.2*smp.sin(smp.pi*(x[0] + x[1] - t*(v[0] + v[1])))
+
+    #Conserved variables
+    u =  [0, 0, 0, 0]
+    u[0] = rho
+    u[1] = u[0]*v[0]
+    u[2] = u[0]*v[1]
+    u[3] = p/(gamma - 1) + 0.5*u[0]*(v[0]*v[0] + v[1]*v[1]) 
+
+    T    = (u[3]/rho - 0.5*(v[0]*v[0] + v[1]*v[1]))/Cv
+
+
+    ##########################
+    #Inviscid
+    ##########################
+    #x Inviscid flux
+    f_inv =  [0, 0, 0, 0]
+    f_inv[0] = rho*v[0]
+    f_inv[1] = rho*v[0]*v[0] + p
+    f_inv[2] = rho*v[0]*v[1]
+    f_inv[3] = (u[3] + p)*v[0] 
+
+    #y Inviscid flux
+    g_inv =  [0, 0, 0, 0]
+    g_inv[0] = rho*v[1]
+    g_inv[1] = rho*v[0]*v[1]
+    g_inv[2] = rho*v[1]*v[1] + p
+    g_inv[3] = (u[3] + p)*v[1] 
+
+    #Time derivative
+    u_t =  [0, 0, 0, 0]
+    for i, u_i in enumerate(u):
+        u_t[i] = smp.diff(u[i], t)
+
+    #x derivative of inviscid flux
+    f_inv_x =  [0, 0, 0, 0]
+    for i, f_i in enumerate(f_inv):
+        f_inv_x[i] = smp.diff(f_inv[i], x[0])
+
+    #y derivative of inviscid flux
+    g_inv_y =  [0, 0, 0, 0]
+    for i, g_i in enumerate(g_inv):
+        g_inv_y[i] = smp.diff(g_inv[i], x[1])
+
+
+    ##########################
+    #Viscous
+    ##########################
+
+    u_x = smp.diff(u[1]/u[0], x[0])
+    u_y = smp.diff(u[1]/u[0], x[1])
+    v_x = smp.diff(u[2]/u[0], x[0])
+    v_y = smp.diff(u[2]/u[0], x[1])
+
+    div = u_x + v_y
+
+    T_x = smp.diff(T, x[0])
+    T_y = smp.diff(T, x[1])
+
+    #x Viscous flux
+    tau11 = mu*(2*u_x - (2.0/3.0)*div)
+    tau12 = mu*(u_y + v_x)
+    f_vis =  [0, 0, 0, 0]
+    f_vis[0] = 0 
+    f_vis[1] = -(tau11) 
+    f_vis[2] = -(tau12) 
+    f_vis[3] = -(v[0]*tau11 + v[1]*tau12 + Pr*T_x)
+
+    #y Viscous flux
+    tau21 = mu*(u_y + v_x)
+    tau22 = mu*(2*v_y - (2.0/3.0)*div)
+    g_vis    =  [0, 0, 0, 0]
+    g_vis[0] = 0 
+    g_vis[1] = -(tau21) 
+    g_vis[2] = -(tau22) 
+    g_vis[3] = -(v[0]*tau21 + v[1]*tau22 + Pr*T_y)
+
+    #Residual
+    resi =  [0, 0, 0, 0]
+    for i, resi_i in enumerate(resi):
+        resi[i] = smp.simplify(u_t[i] + f_inv_x[i] + g_inv_y[i]
+                               +f_vis[i] + g_vis[i]) 
+
+    print(resi)
+
+
+
 def get_ns_3d():
     '''
     Create a manufactured solution for compressbile NS
@@ -158,8 +264,9 @@ def get_ns():
 def problem_mms():
 #    get_navier_stokes_3D()
 #    get_euler_2D()
-    get_ns_3d()
-    get_ns()
+#    get_ns_3d()
+    get_euler_2d()
+#    get_ns()
 
 
 
