28321b3b2705a975e75e61f3a66c850d523afd89
Vikram Singh <vikram.singh@mpimet.mpg.de>
2019-11-19 16:53:21 +0100

    Funciton to get circumcenter and further tests * Added and tested circumcenter calculations * Need to create necessary vectors and gridfunctions to do P^T P

--- cpp/mfem/t1.cpp
+++ cpp/mfem/t1.cpp
@@ -7,6 +7,8 @@ using namespace std;
 using namespace mfem;
 
 
+void getLengths(Vector &A, Vector &B, Vector &C, double &a, double &b, double &c);
+
 // Cosine of angle ABC
 double cosine(Vector &A, Vector &B, Vector &C);
 
@@ -19,7 +21,7 @@ void barycentric(Vector &A, Vector &B, Vector &C, double &p, double &q, double &
 void trilinear(Vector &A, Vector &B, Vector &C, double &alpha, double &beta, double &gamma, Vector &bcn);
 
 // Cartesian coordinates of the circumcenter of triangle ABC
-void circumcenter(Vector &A, Vector &B, Vector &C);
+void circumcenter(Vector &A, Vector &B, Vector &C, Vector &ccn);
 
 
 int main(int argc, char *argv[])
@@ -42,25 +44,68 @@ int main(int argc, char *argv[])
    DG_FECollection fec(order, dim);
 
    FiniteElementSpace *R_space = new FiniteElementSpace(mesh, hdiv_coll);
-   FiniteElementSpace *dg_fes  = new FiniteElementSpace(mesh, &fec);
+   FiniteElementSpace *dg_fes  = new FiniteElementSpace(mesh, &fec, dim);
    
-   GridFunction *coord = new GridFunction(dg_fes);
+   GridFunction *coord = mesh->GetNodes();
+   const FiniteElementSpace *cspace = mesh->GetNodalFESpace();
+   
+   GridFunction ccn_gf(dg_fes);
 
-   DenseMatrix pointmat;
+   Array<int> vdofs;
+   Vector el_vert; 
+   Vector A(dim), B(dim), C(dim);
+   Vector ccn; 
    for (int i = 0; i < mesh->GetNE(); i++)
    {
-     mesh->GetPointMatrix(i, pointmat);
+     cspace->GetElementVDofs(i, vdofs);
+     coord->GetSubVector(vdofs, el_vert);
+     int ndof = el_vert.Size()/dim;
+
+     for (int j = 0; j < dim; j++)
+     {
+       A[j] = el_vert(j*ndof + 0); 
+       B[j] = el_vert(j*ndof + 1); 
+       C[j] = el_vert(j*ndof + 2); 
+     }
+
+     circumcenter(A, B, C, ccn);
+     
+     dg_fes->GetElementVDofs(i, vdofs);
+     ccn_gf.SetSubVector(vdofs, ccn);
    }
 
+   Vector loc(dim), nor(dim);
+   const IntegrationRule *ir ;
+   const FiniteElement *el1, *el2;
    FaceElementTransformations *T;
+   IntegrationPoint eip;
+   
    int nfaces = mesh->GetNumFaces();
    for (int i = 0; i < nfaces; i++)
    {
      T = mesh->GetInteriorFaceTransformations(i);
+
+     el1 = R_space->GetFE(T -> Elem1No);
+     el2 = R_space->GetFE(T -> Elem2No);
+
+     ir = &IntRules.Get(T->FaceGeom, 2*order);
+
+     for (int j = 0; j < ir->GetNPoints(); j++)
+     {
+       const IntegrationPoint &ip = ir->IntPoint(j);
+       
+       T->Loc1.Transform(ip, eip);
+       T->Elem2->Transform(eip, loc);
+
+       T->Face->SetIntPoint(&ip); 
+       CalcOrtho(T->Face->Jacobian(), nor);
+       
+     }
+
    }
 
 
-   delete coord;
+
    delete R_space;
    delete dg_fes;
    delete hdiv_coll;
@@ -70,12 +115,26 @@ int main(int argc, char *argv[])
 }
 
 
+
+void getLengths(Vector &A, Vector &B, Vector &C, double &a, double &b, double &c)
+{
+    Vector temp(A.Size());
+    subtract(B, C, temp);
+    a = temp.Norml2();
+    
+    subtract(A, C, temp);
+    b = temp.Norml2();
+    
+    subtract(A, B, temp);
+    c = temp.Norml2();
+}
+ 
+
 // Cosine of angle ABC
 double cosine(Vector &A, Vector &B, Vector &C)
 {
-    double a = A.Norml2();
-    double b = B.Norml2();
-    double c = C.Norml2();
+    double a, b, c;
+    getLengths(A, B, C, a, b, c);
     return (a*a+c*c-b*b)/(2.*a*c);
 }
 
@@ -101,22 +160,23 @@ void barycentric(Vector &A, Vector &B, Vector &C, double &p, double &q, double &
 // with respect to the triangle ABC are [alpha,beta,gamma]
 void trilinear(Vector &A, Vector &B, Vector &C, double &alpha, double &beta, double &gamma, Vector &bcn)
 {
-    double a = A.Norml2()*alpha;
-    double b = B.Norml2()*beta;
-    double c = C.Norml2()*gamma;
+    double a, b, c;
+    getLengths(A, B, C, a, b, c);
+    a = a*alpha;
+    b = b*beta ;
+    c = c*gamma;
 
     barycentric(A, B, C, a, b, c, bcn);
 }
 
                
 // Cartesian coordinates of the circumcenter of triangle ABC
-void circumcenter(Vector &A, Vector &B, Vector &C)
+void circumcenter(Vector &A, Vector &B, Vector &C, Vector &ccn)
 {
     double cosA = cosine(C,A,B);
     double cosB = cosine(A,B,C);
     double cosC = cosine(B,C,A);
 
-    Vector ccn;
     trilinear(A,B,C,cosA,cosB,cosC, ccn);
 }
 
