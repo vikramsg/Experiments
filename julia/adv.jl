b7711db9cb1d05368a0a6f4c9ef5987b2f7c4712
Vikram Singh <vikram.singh@mpimet.mpg.de>
2020-02-14 18:10:39 +0100

    Now advection has CD2 and one more initial function Julia supports passing functions as arguements

--- julia/adv.jl
+++ julia/adv.jl
@@ -1,54 +1,76 @@
 using PyPlot
 
-function deri(dx, u)
+function upwd(nx, dx, u)
     
   u_der = zeros(size(u)) 
 
-  v     = copy(u)
   for i = 2:nx-1
-    u_der[i] = ( v[i    ] - v[i - 1] )/(dx)
+    u_der[i] = ( u[i ] - u[i - 1] )/(dx)
   end
 
   return u_der
 
 end
 
-function ssp_rk2(dt, dx, u)
- 
-  k = -1*deri(dx, u)
+function cd2(nx, dx, u)
+    
+  u_der = zeros(size(u)) 
 
+  for i = 2:nx-1
+    u_der[i] = ( u[i + 1] - u[i - 1] )/(2*dx)
+  end
+
+  return u_der
+
+end
+
+function ssp_rk2(dt, nx, dx, fn, u)
+ 
+  k = -1*fn(nx, dx, u)
   y = u + dt*k
 
-  k = -1*deri(dx, y)
-  
+  k = -1*fn(nx, dx, y)
   u = 0.5*u + 0.5*y + 0.5*dt*k
 
   return u
 
-
 end
 
 
-nx = 41;
-dx = 2/(nx - 1);
-dt = 0.04
-u  = ones(nx)
+X  = 2.0
+nx = 410;
+dx = X/(nx - 1);
 
-s=Int(0.5/dx); e=Int(1/dx);
+x_grd = range(0, length=nx, stop = X)
 
-u[s:e] = 2*ones(e - s + 1)
+dt = 0.8*dx 
+
+u    = zeros(nx)
+init = 0
+
+if init==0
+  global u  = ones(nx)
+  
+  s=Int(floor(0.5/dx)); e=Int(floor(1/dx));
+  
+  u[s:e] = 2*ones(e - s + 1)
+elseif init==1
+
+  global u  = exp.(-40*(x_grd .- 1.0).^2) 
+
+end 
 
 y  = zeros(nx)
 k  = zeros(nx)
 
-for j = 1:10
+for j = 1:100
 
   # This is stupid. If I don't put the subscript
   # Julia thinks this is a new variable and so
   # changes size, so I have to put global
-  global u = ssp_rk2(dt, dx, u)
+  global u = ssp_rk2(dt, nx, dx, upwd, u)
 
 end
 
-plot(range(0, length=nx, stop = 2), u)
+plot(x_grd, u)
 plt.show()
