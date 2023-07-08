d7fc40e272b78b9bba71a5b2a4017ef79435c850
Vikram <vikramsingh001@gmail.com>
2023-07-08 18:27:56 +0200

    Add get

--- postgres/src/app.py
+++ postgres/src/app.py
@@ -1,9 +1,17 @@
 from fastapi import Depends, FastAPI
 from src.db import get_db
+from sqlalchemy import text
 
 app = FastAPI()
 
 
 @app.post("/tasks")
 async def create_task(task: str, db=Depends(get_db)) -> None:
-    db.execute("INSERT INTO tasks (task) VALUES (:task)", {"task": task})
+    query = text("INSERT INTO tasks (task) VALUES (:task)")
+    db.execute(query, {"task": task})
+
+
+@app.get("/tasks")
+async def get_tasks(db=Depends(get_db)) -> None:
+    query = text("SELECT task FROM tasks")
+    tasks = db.execute(query).fetchall()
