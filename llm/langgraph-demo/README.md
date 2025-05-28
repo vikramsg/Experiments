a095227540050f34009b135ca9eef94ef0461f16
Vikram <vikramsingh001@gmail.com>
2025-05-28 22:06:52 +0200

    Adding example langgraph app

---
+++ llm/langgraph-demo/README.md
@@ -0,0 +1,16 @@
+## Setup
+
+1. Install [ollama](https://github.com/ollama/ollama).
+2. Test using `ollama run gemma3`. This step is going to take some time in the first instance since its going to pull a big model.
+
+### ollama
+
+Some helpful commands. 
+
+```
+ollama run "model"
+
+ollama list 
+
+ollama rm "model"
+```
---
+++ llm/langgraph-demo/main.py
@@ -0,0 +1,29 @@
+from langgraph.prebuilt import create_react_agent
+from langchain_core.tools import tool
+from langchain_ollama import ChatOllama
+from pydantic import BaseModel, Field
+
+
+class MultiplyInputSchema(BaseModel):
+    """Multiply two numbers"""
+
+    a: int = Field(description="First operand")
+    b: int = Field(description="Second operand")
+
+
+@tool("multiply_tool", args_schema=MultiplyInputSchema)
+def multiply(a: int, b: int) -> int:
+    return a * b
+
+
+tools = [multiply]
+
+llm = ChatOllama(model="qwen3:4b")
+
+agent = create_react_agent(
+    llm,
+    tools=tools,
+)
+
+output = agent.invoke({"messages": [{"role": "user", "content": "what's 42 x 7?"}]})
+print(output)
