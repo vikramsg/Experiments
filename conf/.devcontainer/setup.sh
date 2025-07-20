60fd9f6cfe68db2f9430ffe7303136d28dcd9bea
Vikram Singh <vikramsingh001@gmail.com>
2025-07-20 19:05:38 +0000

    Finish devcontainer setup

--- conf/.devcontainer/setup.sh
+++ conf/.devcontainer/setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-cp -r ~/.ssh-owner ~/.ssh
-sudo chown -R vscode:vscode ~/.ssh 2>/dev/null || true
+
+mkdir /home/vscode/.ssh
+sudo cp -r ~/.ssh-owner/* /home/vscode/.ssh/
+sudo chown -R vscode:vscode /home/vscode/.ssh 2>/dev/null || true
 
 # Install Tmux Plugin Manager (TPM) if tmux config exists
 if [ -f "/home/vscode/.tmux.conf" ]; then
@@ -25,6 +27,7 @@ LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/re
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit -D -t /usr/local/bin/
+rm -rf lazygit lazygit.tar.gz
 
 
 echo "Setup complete! You can now use Neovim with your mounted configuration."
