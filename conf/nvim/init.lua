4202fc7c63197bc9552f7916af63f147d191a377
Vikram Singh <vikramsingh001@gmail.com>
2025-07-20 17:25:28 +0200

    Add tmux navigation in nvim init.lua

--- conf/nvim/init.lua
+++ conf/nvim/init.lua
@@ -611,25 +611,6 @@ require("lazy").setup({
 					--  completions whenever it has completion options available.
 					["<C-Space>"] = cmp.mapping.complete({}),
 
-					-- Think of <c-l> as moving to the right of your snippet expansion.
-					--  So if you have a snippet that's like:
-					--  function $name($args)
-					--    $body
-					--  end
-					--
-					-- <c-l> will move you to the right of each of the expansion locations.
-					-- <c-h> is similar, except moving you backwards.
-					["<C-l>"] = cmp.mapping(function()
-						if luasnip.expand_or_locally_jumpable() then
-							luasnip.expand_or_jump()
-						end
-					end, { "i", "s" }),
-					["<C-h>"] = cmp.mapping(function()
-						if luasnip.locally_jumpable(-1) then
-							luasnip.jump(-1)
-						end
-					end, { "i", "s" }),
-
 					-- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
 					--    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
 				}),
@@ -646,6 +627,11 @@ require("lazy").setup({
 				},
 			})
 		end,
+	},
+
+	-- Tmux + vim navigation
+	{
+		"christoomey/vim-tmux-navigator",
 	},
 
 	-- Lazygit for git stuff
